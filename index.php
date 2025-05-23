d79a646bbd385d2dc2266cf85d450df2deb53d22
Nicolas Ruz <ruznicolas176@gmail.com>
2025-05-22 23:19:57 -0400

    jenkifile v3

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-#require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-#use Dotenv\Dotenv;
+use Dotenv\Dotenv;
 
-#$dotenv = Dotenv::createImmutable(__DIR__);
-#$dotenv->load();
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-#$appVar = $_ENV['APP_VAR'] ?? 'variable no definida';
+$appVar = $_ENV['APP_VAR'] ?? 'variable no definida';
 
 echo "Hola desde Jenkins con PHP 8.2 en Docker! 🚀\n";
-#echo "Valor de APP_VAR: {$appVar}\n";
+echo "Valor de APP_VAR: {$appVar}\n";
